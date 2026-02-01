851e3644dc4455e233ebdf36d6960656b8a37334
miguelt <3801282+migueltt@users.noreply.github.com>
2026-02-01 12:46:18 -0500

    Initial commit right after KMP wizard

---
+++ composeApp/src/jvmMain/kotlin/com/acme/main.kt
@@ -0,0 +1,13 @@
+package com.acme
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Acme",
+    ) {
+        App()
+    }
+}
